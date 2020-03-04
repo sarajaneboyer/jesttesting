64d10196d8365d1334b86ea473da76e2afcd59d0
Sara Boyer <sara.jane.boyer@gmail.com>
2020-03-04 16:20:53 -0700

    all done up to array iteratormethods

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,13 @@ import {
   //method,
   originalArray,
   doubleArray,
+  evensOnlyArray,
+  arraySum,
+  allNumbersGreaterThanZero,
+  allPositiveNumbers,
+  someNumbersAreOdd,
+  evensOnlyAndDoubleArray,
+  findItem
 } from '../app'
 
 
@@ -19,5 +26,89 @@ describe('originalArray test', ()=>{
 describe('doubleArray test', ()=>{
   test('should double each value in the array', ()=>{
     expect(doubleArray([1,2,3,4])).toEqual([2,4,6,8])
+  })
+})
+
+describe('evensOnlyArray test', ()=>{
+  test('gives an array with odd values removed', ()=>{
+    expect(evensOnlyArray([6,5,4,3,2,1])).toEqual([6,4,2])
+  })
+})
+
+describe('arraySum test', ()=>{
+  test('sums all number in the array', ()=>{
+    expect(arraySum([1,2,3,4])).toEqual(10)
+  })
+})
+
+
+//this is testing for true
+describe('allNumbersGreaterThanZero test 1', ()=>{
+  test('is every value > 0', ()=>{
+    expect(allNumbersGreaterThanZero([1,2,3,4])).toEqual(true)
+  })
+})
+
+//this is testing for false when all values are <= 0
+describe('allNumbersGreaterThanZero test 2', ()=>{
+  test('is every value > 0', ()=>{
+    expect(allNumbersGreaterThanZero([-3,-2,-1,0])).toEqual(false)
+  })
+})
+
+//this is testing for false when SOME values are <= 0
+describe('allNumbersGreaterThanZero test 3', ()=>{
+  test('is every value > 0', ()=>{
+    expect(allNumbersGreaterThanZero([-1,0,1,2])).toEqual(false)
+  })
+})
+
+// testing the method I accidentally made
+describe('allPositiveNumbers', ()=>{
+  test('returns an array of only positive values', ()=>{
+    expect(allPositiveNumbers([-1,0,1,2,3])).toEqual([1,2,3])
+  })
+})
+
+//testing if someNumbersAreOdd works when all numbers are even
+describe('someNumbersAreOdd test 1', () => {
+  test('are there any odd numbers in the array?', () => {
+    expect(someNumbersAreOdd([-2,0,2,4,6])).toEqual(false)
+  })
+})
+
+//testing if someNumbersAreOdd works when all one number is odd
+describe('someNumbersAreOdd test 2', ()=>{
+  test('are there any odd numbers in the array?', ()=>{
+    expect(someNumbersAreOdd([1,2,3,4])).toEqual(true)
+  })
+})
+
+//testing if someNumbersAreOdd works when the odd number is negative
+describe('someNumbersAreOdd test 3', ()=>{
+  test('are there any odd numbers in the array?',()=>{
+    expect(someNumbersAreOdd([-3,2,4,6])).toEqual(true)
+  })
+})
+
+//testing evensOnlyAndDoubleArray including 0 and negatives
+describe('evensOnlyAndDoubleArray test', () => {
+  test('remove odd numbers and then double array entries', () => {
+    expect(evensOnlyAndDoubleArray([-2,-1,0,1,2,3,4,5])).toEqual([-4, 0, 4, 8])
+  })
+})
+
+
+//testing findItem when item exists
+describe('findItem test 1', () => {
+  test('find an existing item and print it', () => {
+    expect(findItem([1,2,3,4],2)).toEqual(2)
+  })
+})
+
+//testing findItem when item does not exist
+describe('findItem test 2', () => {
+  test('display not found error when an item does not exist', () => {
+    expect(findItem([1,2,3,4],5)).toEqual('not found')
   })
 })
--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 
 let numberArray = [1,2,3,4]
 
+
 export const originalArray = (arr) => {
   return arr
 }
@@ -10,8 +11,8 @@ export const originalArray = (arr) => {
 // return array with double values in array
 // doubleArray(numberArray) // => [2,4,6,8];  hint use map
  export const doubleArray = (arr) => {
-   var newArray = arr.map(function(index, arr) {
-     return index*2
+   var newArray = arr.map(function(el) {
+     return el*2
    })
    return newArray
  }
@@ -19,5 +20,98 @@ export const originalArray = (arr) => {
  // return array with all even numbers
 // evensOnlyArray(numberArray) // => [2,4,6,8];  hint use filter 
 export const evensOnlyArray = (arr) => {
-  
+  function evenFunction(value){
+    return value%2==0
+  }
+  var evenArray = arr.filter(evenFunction);
+  return evenArray
 }
+
+// return array with sum of numbers
+// arraySum(numberArray) // => 10;  hint use reduce
+export const arraySum = (arr) => {
+  function sumFunction(sum,value){
+    return sum + value
+  }
+  return arr.reduce(sumFunction)
+}
+
+// return true if every item greater than 0
+// allNumbersGreaterThanZero(numberArray) // => true; use every
+export const allNumbersGreaterThanZero = (arr) => {
+  function isPositive(el){
+    return el > 0
+  }
+  return arr.every(isPositive)
+}
+
+// I misunderstood the one above and wrote this instead so I'm leaving it here.
+export const allPositiveNumbers = (arr) => {
+  function positiveNumbers(value){
+    return value > 0
+  }
+  return arr.filter(positiveNumbers)
+}
+
+// return true if one or more items are odd
+// someNumbersAreOdd(numberArray) // => true; use some
+export const someNumbersAreOdd = (arr) => {
+  function isNumberOdd(el){
+    return el%2!==0
+  }
+  return arr.some(isNumberOdd)
+}
+
+// return array double and even
+// evensOnlyAndDoubleArray(numberArray) // => [4,8]; // use map and filter
+export const evensOnlyAndDoubleArray = (arr) => {
+  function doubleFunction(el){
+    return el*2
+  }
+  function evenFunction(el){
+    return el%2==0
+  }
+  var evenArray = arr.filter(evenFunction);
+  var evenDoubleArray = evenArray.map(doubleFunction);
+  return evenDoubleArray
+}
+
+// find an item method return 'not found' if not found
+// findItem(numberArray, 1) //=> 1
+export const findItem = (arr, value) => {
+  function findValue(el){
+    return el === value
+  }
+  var newArray = arr.filter(findValue)
+  if (newArray.length === 0) {
+    return 'not found'
+  }
+  else {
+    return newArray[0]
+  }
+}
+
+//sortArray(numberArray) //=> [4,3,2,1] *desc*
+export const sortArray = (arr) => {
+  function whichIsLarger(el1,el2) {
+    return el1-el2
+  }
+  return arr.sort(whichIsLarger)
+}
+
+
+let artists = [
+  {name: 'a', likes:100},
+  {name: 'c', likes:99},
+  {name: 'c', likes:101}, 
+]
+
+
+
+// doubles likes of artist
+doubleLikes(artists)
+// => [
+//   {name: 'a', likes:200},
+//   {name: 'c', likes:198},
+//   {name: 'c', likes:202},  
+// ]; 
